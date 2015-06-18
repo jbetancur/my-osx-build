dd3c35224c446fbdbb3e3c476a671acae338d085
John Betancur <johnnyazee@gmail.com>
2015-06-18 14:25:21 -0400

    enhanced bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,47 @@
-#!/usr/bin/env bash
-set -e
+  #!/usr/bin/env bash
+  set -e
 
-chmod -x ./inventory
+  SUPPORTED_OSX_VERSION=10
+  ACTUAL_OSX_VERSION=$(sw_vers -productVersion | awk -F "." '{print $2}')
 
-if [[ -z $(xcode-select -p) ]];then
-  echo "Info   | Install   | xcode tools"
-  xcode-select --install
-fi
+  if [ "$ACTUAL_OSX_VERSION" -lt $SUPPORTED_OSX_VERSION ]; then
+    echo "You are running OSX version $ACTUAL_OSX_VERSION. You must be running OSX version $SUPPORTED_OSX_VERSION or higher"
+    exit 1
+  fi
 
-if [[ ! -x /usr/local/bin/brew ]]; then
-    echo "Info   | Install   | homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew update
-fi
+  if [[ -z $(xcode-select -p) ]];then
+    echo "Info   | Install   | xcode tools"
+    xcode-select --install
+    sleep 5
+    xtoolsPid=`ps aux | awk '/[I]nstall Command Line Developer Tools.app/ {print $2}'`
+    while (ps -p $xtoolsPid > /dev/null)
+      do sleep 1
+    done
+  fi
 
-if [[ ! -x `which pip` ]]; then
-    echo "Info   | Install   | pip"
-    sudo easy_install pip
-fi
+  if [[ -z $(xcode-select -p) ]];then
+    echo Installation Cancelled - cannot proceed without xcode-select
+    exit 1;
+  fi
 
-if [[ -x `which pip` && ! -x `which ansible` ]]; then
-    echo "Info   | Install   | ansible"
-    sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible
-fi
+  if [[ ! -x `which pip` ]]; then
+      echo "Info   | Install   | pip"
+      sudo easy_install pip
+  fi
 
-if [[ -x `which ansible` ]]; then
-    echo 'Info  | Running Ansible to configure Dev machine'
-    ansible-playbook -i inventory site.yaml --ask-sudo-pass
-fi
+  if [[ -x `which pip` && ! -x `which ansible` ]]; then
+      echo "Info   | Install   | ansible"
+      sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible
+  fi
+
+  if [[ ! -x /usr/local/bin/brew ]]; then
+      echo "Info   | Install   | homebrew"
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+      brew update
+  fi
+
+  if [[ -x `which ansible` ]]; then
+      echo 'Info  | Running Ansible to configure Dev machine'
+      chmod -x ./inventory
+      ansible-playbook -i inventory site.yaml --ask-sudo-pass
+  fi
