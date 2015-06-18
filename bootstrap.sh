550d6d1bee1b6a995074b8fa6b1aa0096e36df68
John Betancur <johnnyazee@gmail.com>
2015-06-18 02:44:48 -0400

    alpha commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,34 +3,28 @@ set -e
 
 chmod -x ./hosts
 
-echo 'Installing XCode Tools'
-
 if [[ -z $(xcode-select -p) ]];then
+  echo "Info   | Install   | xcode tools"
   xcode-select --install
 fi
 
-echo 'Installing Homebrew'
-# Download and install Homebrew
 if [[ ! -x /usr/local/bin/brew ]]; then
     echo "Info   | Install   | homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update
 fi
 
-echo 'Installing Ansible'
-
-# prefer pip for installing python packages over the older easy_install
-#
 if [[ ! -x `which pip` ]]; then
+    echo "Info   | Install   | pip"
     sudo easy_install pip
 fi
 
 if [[ -x `which pip` && ! -x `which ansible` ]]; then
+    echo "Info   | Install   | ansible"
     sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible
 fi
 
-echo 'Running Ansible to configure Dev machine'
-
 if [[ -x `which ansible` ]]; then
+    echo 'Info  | Running Ansible to configure Dev machine'
     ansible-playbook -i hosts main.yaml # --ask-sudo-pass
 fi
